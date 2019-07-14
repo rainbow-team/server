64bb15c9a0f26b26ca89ad05dd235d573a33c375
deepblue <haol@vvipsoft.com>
2019-07-14 17:13:27 +0800

    deepblue

--- src/main/java/com/rainbow/system/domain/extend/SystemMenuExtend.java
+++ src/main/java/com/rainbow/system/domain/extend/SystemMenuExtend.java
@@ -2,6 +2,18 @@ package com.rainbow.system.domain.extend;
 
 import com.rainbow.system.domain.SystemMenu;
 
+import java.util.List;
+
 public class SystemMenuExtend extends SystemMenu{
 
+    //菜单的子菜单
+    private List<SystemMenu> subSystemMenu;
+
+    public List<SystemMenu> getSubSystemMenu() {
+        return subSystemMenu;
+    }
+
+    public void setSubSystemMenu(List<SystemMenu> subSystemMenu) {
+        this.subSystemMenu = subSystemMenu;
+    }
 }
