c910bf68ffcd012295d0a1f34f4117910700c91e
chstyyzq <idealks@163.com>
2019-08-03 13:48:03 +0800

    Revert "Revert "添加菜单管理""
    This reverts commit e64031aeb30648a26e59cfe06f09c767fccb9774.

--- src/main/java/com/rainbow/generator/dao/FileInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import com.rainbow.common.config.MyMapper;
-import domain.FileInfo;
-
-public interface FileInfoMapper extends MyMapper<FileInfo> {
-}
--- src/main/java/com/rainbow/generator/dao/SupervisionSupervisorMapper.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import com.rainbow.common.config.MyMapper;
-import domain.SupervisionSupervisor;
-
-public interface SupervisionSupervisorMapper extends MyMapper<SupervisionSupervisor> {
-}
